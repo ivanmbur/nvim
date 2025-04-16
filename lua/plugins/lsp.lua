7a6503cc8893b25f70ec3c9f216c6568fea5e842
Ivan <ivanmbur@gmail.com>
2025-04-15 23:30:38 -0700

    Got basic lsp working for python

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return{
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "pyright", "lua_ls", "clangd", "julials", "texlab" },
+            automatic_installation = true,
+        })
+        require("mason-lspconfig").setup_handlers {
+            -- The first entry (without a key) will be the default handler
+            -- and will be called for each installed server that doesn't have
+            -- a dedicated handler.
+            function (server_name) -- default handler (optional)
+                require("lspconfig")[server_name].setup {}
+            end,
+        }
+    end,
+}
