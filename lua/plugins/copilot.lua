66421a24766a3aa12df4f9715402f29175c7c336
Ivan <ivanmbur@gmail.com>
2025-04-24 15:55:05 -0700

    replaced copilot.vim by cmp-copilot and now everything plays nicer together

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,16 @@
-return{
-    "github/copilot.vim"
-        }
+return  {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    config = function () require("copilot_cmp").setup() end,
+    dependencies = {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        config = function()
+            require("copilot").setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+            })
+        end,
+    },
+}
+
