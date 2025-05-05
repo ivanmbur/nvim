17a0bf68a99f597e534e4e61932e737495622aff
Ivan <ivanmbur@gmail.com>
2025-05-04 19:01:14 -0700

    Added some colorschemes (going for Kanagawa as my main for a while). Also added mini to add a status bar and some nice commenting features

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,8 @@
+return{
+    'echasnovski/mini.nvim',
+    version = '*',
+    config = function()
+        require("mini.comment").setup({})
+        require("mini.statusline").setup({})
+    end
+}
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,6 +1,6 @@
 return {
     "folke/tokyonight.nvim",
     lazy = false,
-    priority = 1000,
+    priority = 1500,
     opts = {},
 }
