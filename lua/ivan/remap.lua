a15cb7adb4077594c0949c6069abaf919c52bd42
Ivan <ivanmbur@gmail.com>
2025-05-07 02:50:50 -0700

    Changed netrw for oil

--- lua/ivan/remap.lua
+++ lua/ivan/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
