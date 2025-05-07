6a07877f0c6e73c7175a17bdc74dce5bac6ead2e
Ivan <ivanmbur@gmail.com>
2025-05-06 22:13:05 -0700

    update of mason broke my setup. Repaired pyright lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,8 +2,8 @@ return {
     {
         "neovim/nvim-lspconfig",
         dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
+            "mason-org/mason.nvim",
+            "mason-org/mason-lspconfig.nvim",
             "hrsh7th/nvim-cmp",
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
@@ -23,94 +23,105 @@ return {
 
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            require("mason-lspconfig").setup_handlers {
-                function(server_name)
-                    local opts = {
-                        capabilities = capabilities,
-                    }
+            vim.lsp.config["pyright"] = {
+                capabilities = capabilities,
+                settings = {
 
-                    if server_name == "lua_ls" then
-                        opts.settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                                workspace = {
-                                    library = vim.api.nvim_get_runtime_file("", true),
-                                    checkThirdParty = false,
-                                },
-                            },
-                        }
-                    end
-
-                    if server_name == "pyright" then
-                        opts.settings = {
-                            python = {
-                                analysis = {
-                                    extraPaths = { "modules" }, -- <--- your extra folders
-                                },
-                            },
-                        }
-                    end
-
-                    require("lspconfig")[server_name].setup(opts)
-                end,
+                    python = {
+                        analysis = {
+                            extraPaths = { "modules" }, -- <--- your extra folders
+                        },
+                    },
+                }
             }
+                -- require("mason-lspconfig").setup_handlers {
+                --     function(server_name)
+                --         local opts = {
+                --             capabilities = capabilities,
+                --         }
+                --
+                --         if server_name == "lua_ls" then
+                --             opts.settings = {
+                --                 Lua = {
+                --                     diagnostics = {
+                --                         globals = { "vim" },
+                --                     },
+                --                     workspace = {
+                --                         library = vim.api.nvim_get_runtime_file("", true),
+                --                         checkThirdParty = false,
+                --                     },
+                --                 },
+                --             }
+                --         end
+                --
+                --         if server_name == "pyright" then
+                --             opts.settings = {
+                --                 python = {
+                --                     analysis = {
+                --                         extraPaths = { "modules" }, -- <--- your extra folders
+                --                     },
+                --                 },
+                --             }
+                --         end
+                --
+                --         require("lspconfig")[server_name].setup(opts)
+                --     end,
+                -- }
 
-            vim.diagnostic.config({
-                virtual_text = true,
-                signs = true,
-                underline = true,
-                update_in_insert = false,
-                severity_sort = true,
-            })
-
-            -- nvim-cmp setup
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            require("luasnip.loaders.from_vscode").lazy_load()
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "buffer" },
-                    { name = "path" },
-                    { name = "copilot" },
-                    { name = "nvim_lsp_signature_help" }
+                vim.diagnostic.config({
+                    virtual_text = true,
+                    signs = true,
+                    underline = true,
+                    update_in_insert = false,
+                    severity_sort = true,
                 })
-            })
 
-            cmp.setup.cmdline({ "/", "?" }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = "buffer" },
-                },
-            })
+                    -- nvim-cmp setup
+                    local cmp = require("cmp")
+                    local luasnip = require("luasnip")
+                    require("luasnip.loaders.from_vscode").lazy_load()
 
-            cmp.setup.cmdline(":", {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = "path" },
-                }, {
-                        { name = "cmdline" },
-                    }),
-                matching = { disallow_symbol_nonprefix_matching = false },
-            })
-        end,
-    },
-}
+                    cmp.setup({
+                        snippet = {
+                            expand = function(args)
+                                luasnip.lsp_expand(args.body)
+                            end,
+                        },
+                        mapping = cmp.mapping.preset.insert({
+                            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                            ["<C-Space>"] = cmp.mapping.complete(),
+                            ["<C-e>"] = cmp.mapping.abort(),
+                            ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                            ["<C-n>"] = cmp.mapping.select_next_item(),
+                            ["<C-p>"] = cmp.mapping.select_prev_item(),
+                        }),
+                        sources = cmp.config.sources({
+                            { name = "nvim_lsp" },
+                            { name = "luasnip" },
+                            { name = "buffer" },
+                            { name = "path" },
+                            { name = "copilot" },
+                            { name = "nvim_lsp_signature_help" }
+                        })
+                    })
+
+                cmp.setup.cmdline({ "/", "?" }, {
+                    mapping = cmp.mapping.preset.cmdline(),
+                    sources = {
+                        { name = "buffer" },
+                    },
+                })
+
+                cmp.setup.cmdline(":", {
+                    mapping = cmp.mapping.preset.cmdline(),
+                    sources = cmp.config.sources({
+                        { name = "path" },
+                    }, {
+                            { name = "cmdline" },
+                        }),
+                    matching = { disallow_symbol_nonprefix_matching = false },
+                })
+            end,
+            },
+    }
