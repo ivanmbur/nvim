d94f18a6f3e859accfc18dac3379b15420e4eb35
Ivan <ivanmbur@gmail.com>
2025-12-04 17:38:26 -0800

    Changed latex engin to xelatex so that it works with fontspec

--- lua/plugins/vim_slime.lua
+++ lua/plugins/vim_slime.lua
@@ -5,7 +5,7 @@ return {
             vim.cmd([[
             let g:slime_target = "tmux"
             let g:slime_cell_delimiter = "# %%"
-            let g:slime_default_config = {"socket_name": get(split($TMUX, ","), 0), "target_pane": ":.2"}
+            let g:slime_default_config = {"socket_name": get(split($TMUX, ","), 0), "target_pane": ":2"}
             let g:slime_dont_ask_default = 1
             let g:slime_bracketed_paste = 1
             let g:slime_no_mappings = 1
